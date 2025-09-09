872823b781a187155fc0369a354eea7c3ce4d299
Resham <reshamparveen0000@gmail.com>
2025-09-09 20:41:41 +0530

    add icon setting

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -3,7 +3,23 @@ import './Sidebar.css';
 function Sidebar() {
     return ( 
         <>
-        <div>Sidebar</div>
+        <section className='sidebar'>
+            <button>
+                <img src="" alt="" />
+                <i className="fa-solid fa-pen-to-square"></i>
+            </button>
+
+            <ul>
+                <li>history1</li>
+                <li>history2</li>
+                <li>history3</li>
+                <li>history4</li>
+            </ul>
+
+            <div className='sign'>
+                <p>by Resham &hearts;</p>
+            </div>
+        </section>
         </>
      );
 }
